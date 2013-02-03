79c437603cd58b6de1bc834e60f8147ccccd50fd
carlosjln <carlosjln01@gmail.com>
2013-02-02 20:39:44 -0430

    updating files location

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "DummyExtensions" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Some helpful extensions :)" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany( "Carlos J. López - twitter/carlosjln" )]
 [assembly: AssemblyProduct( "DummyExtensions" )]
 [assembly: AssemblyCopyright( "Copyright ©  2013" )]
 [assembly: AssemblyTrademark( "" )]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "1.0.*" )]
 [assembly: AssemblyFileVersion( "1.0.0.0" )]
